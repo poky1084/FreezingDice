22d2c9b41c824fd9dc92f6fe68b4644f94c69faa
Poky1084 <nanohtcop@gmail.com>
2023-01-30 17:28:50 +0100

    Add files via upload

--- setup.py
+++ setup.py
@@ -5,4 +5,4 @@ setup(
     name = "pydice",
     version = "0.1",
     description = "lp",
-    executables = [Executable("freezingdice2.py", base="Win32GUI")] )
+    executables = [Executable("freezingdice.py", base="Win32GUI")] )
